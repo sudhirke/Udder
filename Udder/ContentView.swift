b73b6e7ae5291d4ecc5d91ccc377a17d7526b095
Sudhir Kesharwani <sudhir.kesharwani@gmail.com>
2023-05-23 11:27:33 +0530

    added spxpert image

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image("White-Green")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Text("Sudhir Kesharwani")
+                .fontWeight(.heavy)
+                .badge("iOS-Expert")
         }
         .padding()
     }
