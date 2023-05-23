c62384ee5e38face8d5e97beb04f71060a316419
Sudhir Kesharwani <sudhir.kesharwani@gmail.com>
2023-05-23 11:21:49 +0530

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Sudhir Kesharwani on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
